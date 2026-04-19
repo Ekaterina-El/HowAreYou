9ef2ab03f6f8c8ce8dacbdb062a50e4ba4f23932
Ekaterina-El <hackerussia@yandex.ru>
2026-04-19 16:30:54 +0500

    Init commit

---
+++ app/src/main/java/com/ekaterinael/howareyou/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ekaterinael.howareyou
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            // TODO: add content
+        }
+    }
+}
